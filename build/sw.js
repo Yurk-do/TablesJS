4ed3f055871a39231649f6ba56f2cec90febdac4
Yuru Dobranov <yuru.dobranov@intexsoft.by>
2024-05-31 16:43:15 +0300

    build

---
+++ build/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'scope-cache-v1';
+const urlsToCache = [
+    '/',
+    'favicon.ico',
+    'logo192.png',
+    'logo512.png',
+    'manifest.json',
+    'index.html',
+    'static/js/bundle.js',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache).then(() => {
+        console.log("cached!")
+        this.skipWaiting();
+      });
+    }),
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheKeeplist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(
+      (keyList) => Promise.all(
+        keyList
+          .filter((key) =>!cacheKeeplist.includes(key))
+          .map((key) => caches.delete(key)
+        )
+      ),
+    ),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request);
+  event.respondWith(
+    caches.match(event.request.url).then((cachedResponse) => {
+      console.log(cachedResponse);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
+});

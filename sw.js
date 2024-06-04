fa8a3d946e8373a1c4171cfbec2543e745118fde
Yuru Dobranov <yuru.dobranov@intexsoft.by>
2024-06-04 15:57:40 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'scope-cache-v2';
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-  window.location.hostname === '[::1]' ||
-  window.location.hostname.match(
+  self.location.hostname === 'localhost' ||
+  self.location.hostname === '[::1]' ||
+  self.location.hostname.match(
     /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
   ),
 );

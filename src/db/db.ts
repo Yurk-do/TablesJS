c44f9a943fde22866be772f2df0c9c38ba745906
Yuru Dobranov <yuru.dobranov@intexsoft.by>
2024-06-04 13:13:49 +0300

    correct indexedDb

--- src/db/db.ts
+++ src/db/db.ts
@@ -18,19 +18,40 @@ export const initDB = (): Promise<boolean> =>
   new Promise((resolve, reject) => {
     const request = indexedDB.open(dbName, dbVersion);
 
-    request.onupgradeneeded = () => {
+    request.onupgradeneeded = (event) => {
       const db = request.result;
+      if (event.oldVersion === 1) {
+        db.deleteObjectStore(Stores.Tables);
+      }
       db.createObjectStore(Stores.Tables, { keyPath: 'name' });
     };
 
     request.onsuccess = () => {
       const db = request.result;
+
+      db.onversionchange = () => {
+        db.close();
+        alert('База данных устарела, пожалуйста, перезагрузите страницу.');
+      };
+
       const { version } = db;
       console.log('db Initialized with version: ', version);
       resolve(true);
     };
 
-    request.onerror = () => {
+    request.onerror = (event) => {
+      if (request.error?.name === 'VersionError') {
+        const deleteRequest = indexedDB.deleteDatabase(dbName);
+        deleteRequest.onsuccess = () => {
+          console.log('Database deleted successfully');
+          return initDB();
+        };
+
+        deleteRequest.onerror = () => {
+          console.log('Failed to delete database');
+          reject();
+        };
+      }
       reject(new Error('Failed to initialize the database'));
     };
   });
